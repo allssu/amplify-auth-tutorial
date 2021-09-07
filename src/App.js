11775f8c41a322be5c3469c60e37dc33d0e5cabb
allssu <zxcvb6615@gmail.com>
2021-09-08 01:35:00 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Amplify from "aws-amplify"
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react"
+
+// 하기 코드의 경우, Amplify를 통해서 aws-exports.js가 만들어진 후에 사용합니다.
+// import awsconfig from "./aws-exports"
+// Amplify.configure(awsconfig)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AmplifySignOut />
+      메인화면
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
